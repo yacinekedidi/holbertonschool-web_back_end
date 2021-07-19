cc7d0bb6e2e4fcb4046c31aad3fa9ce1ef849b4c
yacinekedidi <kedidiyacine@gmail.com>
2021-07-19 19:01:04 +0100

    task-4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""[a simple module containning a couple of type annotated variables]"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
