0d5acab2a195419513943fdcf372b4cef4bdf69c
yacinekedidi <kedidiyacine@gmail.com>
2021-07-19 18:47:47 +0100

    task-1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""[summary]"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ takes a string str1 and a string str2
+    as arguments and returns a concatenated string
+
+    Args:
+        str1 (str): first string
+        str2 (str): second string
+
+    Returns:
+        str: concatenated string
+    """
+    return str1 + str2
