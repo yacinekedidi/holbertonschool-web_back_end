8e7a13f48d2173bab2f94e23550656189296048f
yacinekedidi <kedidiyacine@gmail.com>
2021-07-19 23:23:22 +0100

    task-10

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,6 +4,15 @@ from typing import Optional, Sequence, Any
 
 
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """[summary]
+
+    Args:
+        lst (Sequence[Any]): [sequence on any types]
+
+    Returns:
+        Optional[Any]: [equivalent to Union[Any, None]]
+        which means could return either any type or none
+    """
     if lst:
         return lst[0]
     else:
