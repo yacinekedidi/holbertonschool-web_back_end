19099f748b7e4f871cf3b1a0f6b4aa869139ce2c
yacinekedidi <kedidiyacine@gmail.com>
2021-07-19 18:42:30 +0100

    first task

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""a simple module containing a method"""
+
+
+def add(a: float, b: float) -> float:
+    """[summary]
+
+    Args:
+        a (float): first float
+        b (float): second float
+
+    Returns:
+        float: sum of a and b
+    """
+    return a + b
