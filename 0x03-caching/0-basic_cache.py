0a86fbdf75782695f337e1587debbc1f0b570f5e
yacinekedidi <kedidiyacine@gmail.com>
2021-07-24 02:28:30 +0100

    task-0

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -1,35 +1,42 @@
 #!/usr/bin/env python3
-""""Module"""
+""""BasicCache Module"""
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """[summary]
+    """[basic cache strategy]
 
     Args:
-        BaseCaching ([type]): [description]
+        BaseCaching ([class]): [parent]
     """
     def __init__(self):
-        """[summary]
+        """[constructor]
         """
         super().__init__()
 
     def put(self, key, item):
-        """[summary]
+        """[- assign to the dictionary self.cache_data the item value
+            for the key @key.
+            - If key or item is None, this method should not do anything.]
 
         Args:
-            key ([type]): [description]
-            item ([type]): [description]
+            key ([Any]): [@self.cache_data key]
+            item ([type]): [new value]
         """
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
-        """[summary]
+        """[- Must return the value in self.cache_data linked to key.
+            - If key is None or if the key doesn’t exist in self.cache_data,
+            return None.]
 
         Args:
-            key ([type]): [description]
+            key ([Any]): [@self.cache_data key]
+
+        Returns:
+            [Any]: [the value in self.cache_data linked to key.]
         """
         if key is None or key not in self.cache_data.keys():
             return None
