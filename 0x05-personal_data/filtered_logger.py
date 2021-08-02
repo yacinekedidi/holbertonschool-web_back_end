df3baeae5c686126438a23f8e6a9cfc9065c645e
yacinekedidi <kedidiyacine@gmail.com>
2021-08-02 07:58:05 +0100

    task-0

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -18,6 +18,12 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         str: [description]
     """
     msg_list = message.split(separator)[:-1]
-    return (';'.join(re.sub(i.split('=')[1], redaction, i)
-            if i.split('=')[0] in fields else i
-            for c, i in enumerate(msg_list)) + ';')
+    return (
+        ';'.join(
+            re.sub(i.split('=')[1], redaction, i)
+            if i.split('=')[0] in fields
+            else i
+            for c, i in enumerate(msg_list)
+        )
+        + ';'
+    )
