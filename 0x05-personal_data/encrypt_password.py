9e378b0fdb569252c74e0d87698f8cdb50f6dee7
yacinekedidi <kedidiyacine@gmail.com>
2021-08-02 18:21:06 +0100

    task-6

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -5,5 +5,11 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """[summary]
+
+    Args:
+        password (str): [description]
+
+    Returns:
+        bytes: [description]
     """
     return bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
