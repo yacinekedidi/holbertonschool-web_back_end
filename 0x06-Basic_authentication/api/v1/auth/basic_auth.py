7c17a2e73dd1e93d70c0f490aac7339b77d01d30
yacinekedidi <kedidiyacine@gmail.com>
2021-08-05 12:29:02 +0100

    task-6

---
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""[Module]"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """[summary]
+
+    Args:
+        Auth ([type]): [description]
+    """
+    pass
