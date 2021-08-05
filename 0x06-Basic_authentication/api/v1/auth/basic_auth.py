45465045cf766f9a964d8fb76c6b1d09e8c681cf
yacinekedidi <kedidiyacine@gmail.com>
2021-08-05 14:25:14 +0100

    task-9

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """[Module]"""
 
+from typing import Tuple
 from api.v1.auth.auth import Auth
 import base64
 
@@ -48,3 +49,23 @@ class BasicAuth(Auth):
                 base64_authorization_header).decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+                self, decoded_base64_authorization_header:
+                str) -> Tuple[str, str]:
+        """[summary]
+
+        Args:
+            decoded_base64_authorization_header (str): [description]
+
+        Returns:
+            Tuple[str, str]: [description]
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+        if decoded_base64_authorization_header.find(":") == -1:
+            return None, None
+        email, password = decoded_base64_authorization_header.split(":")
+        return email, password
