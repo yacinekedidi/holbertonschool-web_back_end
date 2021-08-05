f1a325a20bad399949a01b23b8d1d3605586dcc7
yacinekedidi <kedidiyacine@gmail.com>
2021-08-05 13:19:41 +0100

    task-7

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,11 @@ class BasicAuth(Auth):
     Args:
         Auth ([type]): [description]
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
