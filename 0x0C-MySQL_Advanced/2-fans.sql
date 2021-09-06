259c9fbfc31613f0617940e1e8068b42980f5cfd
yacinekedidi <kedidiyacine@gmail.com>
2021-09-06 14:15:20 +0100

    task-2

--- 0x0C-MySQL_Advanced/2-fans.sql
+++ 0x0C-MySQL_Advanced/2-fans.sql
@@ -1,2 +1,5 @@
 -- a SQL script that ranks country origins of bands,
-SELECT origin, SUM(fans) AS nb_fans FROM metal_bands GROUP BY origin ORDER nb_fans DESC;
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
