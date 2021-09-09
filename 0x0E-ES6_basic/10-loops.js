74a4062d447d7225721102d2fb33e4355455ce81
yacinekedidi <kedidiyacine@gmail.com>
2021-09-09 16:01:54 +0100

    task-10

---
+++ 0x0E-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const item of array) {
+    newArray.push(`${appendString}${item}`);
+  }
+  return newArray;
+}
