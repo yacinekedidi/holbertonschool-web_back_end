3bb6f68e246f4fef3fa62c5a1d857787fa789767
yacinekedidi <kedidiyacine@gmail.com>
2021-09-14 11:57:18 +0100

    task-06

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -3,5 +3,5 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName),
-    uploadPhoto(fileName)]).then((data) => (data.map((result) => (result.status === 'fulfilled' ? ({ status: result.status, value: result.value }) : ({ status: result.status, value: result.reason })))));
+    uploadPhoto(fileName)]);
 }
