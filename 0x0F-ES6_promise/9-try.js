854e5abb90afbdc06d45c503f84d524b82deaeb5
yacinekedidi <kedidiyacine@gmail.com>
2021-09-14 12:29:30 +0100

    task-09

--- 0x0F-ES6_promise/9-try.js
+++ 0x0F-ES6_promise/9-try.js
@@ -2,9 +2,8 @@ export default function guardrail(mathFunction) {
   let res;
   try {
     res = mathFunction();
-
   } catch (err) {
-      res = `Error: ${err.message}`;
+    res = `Error: ${err.message}`;
   }
   return [res, 'Guardrail was processed'];
 }
