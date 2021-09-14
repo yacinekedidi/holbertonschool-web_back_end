5014aa68987edbb9ecf041bfdc6d1c87a6c1ebd2
yacinekedidi <kedidiyacine@gmail.com>
2021-09-14 13:01:39 +0100

    task-06

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -2,6 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName),
-    uploadPhoto(fileName)]).then((data) => data);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((data) => {
+      const arr = data.map((result) => (result.status === 'rejected'
+        ? ({ ...result, value: result.reason })
+        : ({ ...result })
+      ));
+      return arr;
+    });
 }
