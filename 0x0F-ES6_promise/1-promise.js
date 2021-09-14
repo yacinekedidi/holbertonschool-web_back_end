58cf6e60e7870381daa3e80c6e3bb59b4c4c6008
yacinekedidi <kedidiyacine@gmail.com>
2021-09-14 10:12:31 +0100

    task-1

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -1,9 +1,8 @@
 const getFullResponseFromAPI = (success) =>
-  // eslint-disable-next-line implicit-arrow-linebreak
-  new Promise((resolve, reject) => {
-    if (success) resolve({ status: 200, body: 'Success' });
-    // eslint-disable-next-line prefer-promise-reject-errors
-    else reject({ error: 'The fake API is not working currently' });
-  });
+  new Promise((resolve, reject) =>
+    success
+      ? resolve({ status: 200, body: 'Success' })
+      : reject(new Error('The fake API is not working currently'))
+  );
 
 export default getFullResponseFromAPI;
