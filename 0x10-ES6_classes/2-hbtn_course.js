0d854609d888ddbf775b593f296180254d9b2f56
yacinekedidi <kedidiyacine@gmail.com>
2021-09-16 18:08:39 +0100

    task-02

---
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,38 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') throw TypeError('Name must be a string');
+    if (typeof length !== 'number') throw TypeError('Length must be a number');
+    const isBad = students.some((student) => typeof student !== 'string');
+    if (!Array.isArray(students) && isBad) {
+      throw TypeError('Students must be an array of strings');
+    }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    this._name = val;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(val) {
+    this._length = val;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(arr) {
+    this._students = arr;
+  }
+}
