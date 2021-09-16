37b9b5d6b48465632b43fd80ababd653f17d9f43
yacinekedidi <kedidiyacine@gmail.com>
2021-09-16 18:47:29 +0100

    task-03

--- 0x10-ES6_classes/3-currency.js
+++ 0x10-ES6_classes/3-currency.js
@@ -20,7 +20,7 @@ export default class Currency {
     this._name = val;
   }
 
-  static displayFullCurrency() {
-    return `${this._name}(${this._code})`;
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
   }
 }
