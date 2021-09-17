d5afdedd6c4d99cc3bb2216e3e1a6a96d8e29efb
yacinekedidi <kedidiyacine@gmail.com>
2021-09-17 11:10:58 +0100

    task-10

---
+++ 0x10-ES6_classes/10-car.js
@@ -0,0 +1,13 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return Object.assign(Object.create(this), {
+      ...new Car(undefined, undefined, undefined),
+    });
+  }
+}
