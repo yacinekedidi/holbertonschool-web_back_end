6dc52a28ea4e4a2d85465d784be2a7fd071eda46
yacinekedidi <kedidiyacine@gmail.com>
2021-09-20 22:39:31 +0100

    tasks

---
+++ 0x11-ES6_data_manipulation/4-main.js
@@ -0,0 +1,16 @@
+/* eslint-disable comma-dangle */
+import getListStudents from './0-get_list_students';
+import updateStudentGradeByCity from './4-update_grade_by_city';
+
+console.log(
+  updateStudentGradeByCity(getListStudents(), 'San Francisco', [
+    { studentId: 5, grade: 97 },
+    { studentId: 1, grade: 86 },
+  ])
+);
+
+console.log(
+  updateStudentGradeByCity(getListStudents(), 'San Francisco', [
+    { studentId: 5, grade: 97 },
+  ])
+);
---
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,17 @@
+export default function updateStudentGradeByCity(
+  students = [],
+  city = '',
+  // eslint-disable-next-line comma-dangle
+  newGrades = []
+) {
+  return students
+    .filter((student) => student.location === city)
+    .map((studentInCity) => {
+      let grade = 'N/A';
+      // eslint-disable-next-line array-callback-return
+      newGrades.filter((studentGrade) => {
+        if (studentGrade.studentId === studentInCity.id) grade = grade.grade;
+      });
+      return { ...studentInCity, grade };
+    });
+}
