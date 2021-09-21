7fd58a2421aad8ce45f07de7e645c9de78298519
yacinekedidi <kedidiyacine@gmail.com>
2021-09-21 17:25:02 +0100

    task

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -10,7 +10,9 @@ export default function updateStudentGradeByCity(
       let grade = 'N/A';
       // eslint-disable-next-line array-callback-return
       newGrades.filter((studentGrade) => {
+        // eslint-disable-next-line curly
         if (studentGrade.studentId === studentInCity.id)
+          // eslint-disable-next-line nonblock-statement-body-position
           grade = studentGrade.grade;
       });
       return { ...studentInCity, grade };
