2462b0fc2a184cd8526532662c92bdc7b21ad092
yacinekedidi <kedidiyacine@gmail.com>
2021-09-21 22:07:48 +0100

    task

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!startString) return '';
+  if (!startString || typeof startString !== 'string') return '';
   const arr = [];
   set.forEach((elem) => {
     if (elem.startsWith(startString)) {
