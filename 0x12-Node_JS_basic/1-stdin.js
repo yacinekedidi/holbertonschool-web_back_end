179ede1be699660ad00146ef29c1b70a86120a36
yacinekedidi <kedidiyacine@gmail.com>
2021-09-25 07:29:58 +0100

    task

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,14 +1,10 @@
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-readline.question(
-  'Welcome to Holberton School, what is your name?\n',
-  (name) => {
-    console.log(`Your name is: ${name}`);
-    readline.close();
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) process.stdout.write(`Your name is: ${name}`);
+  if (process.stdin.isTTY) process.exit();
+  else {
     console.log('This important software is now closing');
-    // eslint-disable-next-line comma-dangle
+    process.exit();
   }
-);
+});
