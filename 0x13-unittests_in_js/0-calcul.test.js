56bd68f65cd42c5c37088fb1a0ac2fd550e5a1a6
yacinekedidi <kedidiyacine@gmail.com>
2021-10-18 02:44:25 +0100

    test

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -4,7 +4,7 @@ const calculateNumber = require('./0-calcul');
 describe('calculateNumber', () => {
   it('calculateNumber(1, 3)', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3), 5);
     assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
