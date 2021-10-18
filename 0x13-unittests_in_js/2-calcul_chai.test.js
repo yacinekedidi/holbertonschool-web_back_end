c6999e5d667319e77b36a8e676fd84706c118f39
yacinekedidi <kedidiyacine@gmail.com>
2021-10-18 03:29:55 +0100

    test

--- 0x13-unittests_in_js/2-calcul_chai.test.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -1,5 +1,5 @@
 const chai = require("chai")
-const calculateNumber = require('./1-calcul_chai')
+const calculateNumber = require('./2-calcul_chai')
 
 describe('calculateNumber', () => {
     it('SUM', () => {
