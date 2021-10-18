0f16cb0186d3031bd746d0e861e2f0d10b3af7fa
yacinekedidi <kedidiyacine@gmail.com>
2021-10-18 02:41:18 +0100

    test

---
+++ 0x13-unittests_in_js/0-calcul.js
@@ -0,0 +1,5 @@
+function calculateNumber(a = 0, b = 0) {
+  return Math.round(a) + Math.round(b);
+}
+
+module.exports = calculateNumber;
---
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+  it('calculateNumber(1, 3)', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  })
+});
