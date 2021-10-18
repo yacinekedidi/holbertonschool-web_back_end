f00f4d41849ae0381aca17639a8d963713e871f1
yacinekedidi <kedidiyacine@gmail.com>
2021-10-18 19:28:14 +0100

    test

--- 0x13-unittests_in_js/6-payment_token.test.js
+++ 0x13-unittests_in_js/6-payment_token.test.js
@@ -5,9 +5,9 @@ describe('getPaymentTokenFromAPI', () => {
   it('', (done) => {
     getPaymentTokenFromAPI(true)
       .then((res) => {
-        chai.expect(res).to.equal({ data: 'Successful response from the API' });
+        chai.expect(res.data).to.equal('Successful response from the API');
+        done();
       })
-      .catch((err) => console.error(err))
-      .finally(() => done());
+      .catch((err) => done(err));
   });
 });
