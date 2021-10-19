c8d8f07d274a22475187d9f5887291db8482e50c
yacinekedidi <kedidiyacine@gmail.com>
2021-10-19 05:20:32 +0100

    test

--- 0x13-unittests_in_js/9-api/api.js
+++ 0x13-unittests_in_js/9-api/api.js
@@ -2,6 +2,9 @@ const app = require('express')();
 
 const PORT = 7865;
 
+app.get('/', (req, res) =>
+  res.status(200).send('Welcome to the payment system')
+);
 app.get('/cart/:id([0-9]+)', (req, res) => {
   const { id } = req.params;
   return res.status(200).send(`Payment methods for cart ${id}`);
--- 0x13-unittests_in_js/9-api/api.test.js
+++ 0x13-unittests_in_js/9-api/api.test.js
@@ -2,6 +2,15 @@ const chai = require('chai');
 const request = require('request');
 
 describe('test suite for basic integration testing', () => {
+  it('GET /', (done) => {
+    request('http://localhost:7865', (err, response, body) => {
+      chai.expect(err).to.be.null;
+      chai.expect(response.statusCode).to.equal(200);
+      chai.expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
   it('GET /cart/:id with valid id', (done) => {
     request('http://localhost:7865/cart/12', (err, response, body) => {
       chai.expect(err).to.be.null;
